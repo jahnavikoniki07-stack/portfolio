551b58ce9d35c73edf1f0e58a1382fbb92340893
jahnavikoniki07-stack <jahnavikoniki07@gmail.com>
2025-12-29 12:31:42 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,64 @@
+export const PORTFOLIO_DATA = {
+  name: "KONIKI JAHNAVI",
+  title: "Full Stack Developer",
+  bio: "I build beautiful and functional web experiences",
+  email: "konikijahnavi07@gmail.com",
+  phone: "8639980790",
+  location: "Pudipatla,Tirupathi,AP",
+  
+  social: {
+    github: "https://github.com/jahnavikoniki07-stack",
+    linkedin: "https://www.linkedin.com/in/koniki-jahnavi-8304392a7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitter: "https://www.instagram.com/chowdary_075?igsh=MTl1YW50OXhoYzFvOQ==",
+    portfolio: "https://yourportfolio.com"
+  },
+
+  about: {
+    title: "About Me",
+    description: `I'm a passionate full-stack developer with building web applications. 
+    I love creating elegant solutions to complex problems and staying updated with modern technologies.`,
+    skills: ["React", "Node.js", "Python","HTML", "JavaScript", "Tailwind CSS", "MongoDB", "PostgreSQL"]
+  },
+
+  projects: [
+    {
+      id: 1,
+      title: "MERN STACK",
+      description: "I have learned the MERN stack,I did a frontend and backend wed by usning MERN stack ",
+      tech: ["React", "Node.js", "MongoDB", "Stripe"],
+      link: "https://example.com",
+      github: "https://github.com/jahnavikoniki07-stack/Jaanu"
+    },
+    {
+      id: 2,
+      title: "Blog",
+      description: "I have devloped an Blog it have both frontend and backend which will allow create,update,delete blog",
+      tech: ["React", "Firebase", "Tailwind CSS"],
+      link: "https://example.com",
+      github: "https://github.comhttps://github.com/jahnavikoniki07-stack/bolg"
+    },
+    {
+      id: 3,
+      title: "AI apps",
+      description: "Devloped AI apps bby using online AI apps by this I can get to know how to give Prompt to AI and I will develop the apps in less time",
+      tech: ["React", "Chart.js", "Node.js","HTML"],
+      link: "https://example.com",
+      github: "https://github.com/jahnavikoniki07-stack/volg"
+    }
+  ],
+
+  experience: [
+    {
+      college: "VEMU INSTITUTE OF TECHNOLOGY",
+      academicyear: "2023-2027",
+      brach: "Computer science and engineering",
+      percentage: "90.3"
+    },
+    {
+      college: "Sri chaitanya girls colleg",
+      academicyear: "2021-2023",
+      branch: "MPC",
+      percentage: "90"
+    }
+  ]
+};
